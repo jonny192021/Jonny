428699baa9d41ad2d272a10c84eda16eae49830f
jonny192021 <126866021+jonny192021@users.noreply.github.com>
2023-03-25 08:25:37 +0530

    Update SelectByIndex.java

--- SelectByIndex.java
+++ SelectByIndex.java
@@ -13,46 +13,8 @@ public class SelectByIndex {
 
 	public static void main(String[] args) {
 
-
-		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      System.out.println("My name is shree") ;
 		
-//		driver.get("https://www.facebook.com/signup");
-//		WebElement allmonth = driver.findElement(By.id("month"));
-//        Select s=new Select(allmonth);
-////        s.selectByIndex(4);
-//        for (int i = 0; i < 12; i++) {
-//			s.selectByIndex(i);
-//		}
-		driver.get("https://www.orangehrm.com/orangehrm-30-day-trial/");
-		WebElement country = driver.findElement(By.name("Country"));
-        Select s=new Select(country);
-            List<WebElement> allcountry = s.getOptions();
-	for (WebElement webElement : allcountry) {
-		System.out.println(webElement.getText());
-		s.selectByVisibleText(webElement.getText());System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-//		driver.get("https://www.facebook.com/signup");
-//		WebElement allmonth = driver.findElement(By.id("month"));
-//        Select s=new Select(allmonth);
-////        s.selectByIndex(4);
-//        for (int i = 0; i < 12; i++) {
-//			s.selectByIndex(i);
-//		}
-		driver.get("https://www.orangehrm.com/orangehrm-30-day-trial/");
-		WebElement country = driver.findElement(By.name("Country"));
-        Select s=new Select(country);
-            List<WebElement> allcountry = s.getOptions();
-	for (WebElement webElement : allcountry) {
-		System.out.println(webElement.getText());
-		s.selectByVisibleText(webElement.getText());
 	}
-}
-}
 
-	}
-}
 }
